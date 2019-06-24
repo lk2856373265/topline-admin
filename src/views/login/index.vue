e9ab9b02b907a0f8089c1753d3938e9945d5d25c
linkun <2856373265@qq.com>
2019-06-24 09:15:18 +0800

    '发送验证码，请求获取初始验证码的参数'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,75 +1,76 @@
 <template>
   <div class="login-wrap">
-<el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
-      </el-form-item>
-</el-form>
+    <div class="login-form-weap">
+  <div class='login-head'>
+    <img src="./logo_index.png" alt="黑马头条">
   </div>
+     <div class='login-form'>
+    <el-form ref="form" :model="form" >
+        <el-form-item >
+          <el-input v-model="form.mobile" placeholder='请输入手机号'></el-input>
+        </el-form-item>
+      <el-form-item >
+        <el-col :span=10><el-input v-model="form.code" placeholder='验证码'></el-input></el-col>
+        <el-col :span=10 :offset=4 ><el-button @click="handleSendCode">发送验证码</el-button></el-col>
+        <!-- 支持栅格布局 -->
+        </el-form-item>
+        <el-form-item>
+          <!-- 给组件加class，会作用到他的根元素 -->
+          <el-button type="primary" class="btn-login" @click="onSubmit">登录</el-button>
+    </el-form-item>
+</el-form>
+     </div>
+  </div>
+    </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'AppLogin',
   data () {
     return {
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        mobile: '17862066801',
+        code: ''
       }
     }
   },
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang='less' scoped>
+.login-wrap {
+ height: 100%;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ background-color: #ccc;
+ border-radius: 20px;
+ .login-head {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 15px;
+}
+}
+.login-form-weap {
+  background-color: #fff;
+  padding: 50px;
+  .btn-login {
+    width: 100%;
+  }
+}
 </style>
