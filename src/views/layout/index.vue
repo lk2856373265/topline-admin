3b6eb4f136cc88b2857846b047ae9250d7f01de8
linkun <2856373265@qq.com>
2019-06-27 10:49:03 +0800

    '创建并配置路由'

---
+++ src/views/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <AppAside/>
+    <AppHeader/>
+    <!--这里的内容是home，评论文章，发布管理...... -->
+  </div>
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
