e58dc51af93c7439a81cd743345e6581e0597f3e
linkun <2856373265@qq.com>
2019-06-28 16:02:46 +0800

    '使用nprogress实现导航进度条动画'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
+import nprogress from 'nprogress'
 import Router from 'vue-router'
-
 Vue.use(Router)
 
 const router = new Router({
@@ -13,7 +13,7 @@ const router = new Router({
       children: [ // 所有 children 路由都显示到父路由的router-view 中
         {
           name: 'home',
-          path: '/', // 它就是layout 的默认子路由
+          path: '', // 它就是layout 的默认子路由
           component: () => import('@/views/home')
         },
         {
@@ -37,12 +37,13 @@ const router = new Router({
  * next 允许通过的方法
  */
 router.beforeEach((to, from, next) => {
-  const userinfo = window.localStorage.getItem('user_info')
+  nprogress.start()
+  const userInfo = window.localStorage.getItem('user_info')
   // 如果是非登录页面（/login）,判断其登录状态
   // console.log(to)
   if (to.path !== '/login') {
-    // 如果没有登录，让其跳转但登录页
-    if (!userinfo) {
+    // 如果没有登录，让其跳转到登录页
+    if (!userInfo) {
       next({ name: 'login' })
     } else {
       // 如果登录了，则允许通过
@@ -50,12 +51,18 @@ router.beforeEach((to, from, next) => {
     }
   } else {
     // 如果登录了，就不允许访问登录页了
-    if (userinfo) {
+    if (userInfo) {
       next(false)
     } else {
       // 没有登录，才允许访问登录页
       next()
     }
   }
+})
+/**
+ * 路由导航完成时会进入这里
+ */
+router.afterEach((to, from) => {
+  nprogress.done()
 })
 export default router
