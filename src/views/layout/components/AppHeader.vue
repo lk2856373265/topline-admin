c1733a9180024d9c3e67c03033de106e42f2b38b
linkun <2856373265@qq.com>
2019-07-03 22:06:43 +0800

    '用户退出'

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -9,7 +9,12 @@
         </span>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>账户设置</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <!--
+            给组件注册原生事件 ：@原生事件类型.native="处理函数"
+            .native修饰符
+            该事件会被注册给组件的根元素
+           -->
+          <el-dropdown-item @click.native="handleLogout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -26,6 +31,31 @@ export default {
   },
   created () {
     this.user_info = JSON.parse(window.localStorage.getItem('user_info'))
+  },
+  methods: {
+    handleLogout () {
+      this.$confirm('确定退出吗', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 清楚本地用户中的存储信息 user_info
+        window.localStorage.removeItem('user_info')
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+        // 跳转到登录页面
+        this.$router.push({
+          name: 'login'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
   }
 }
 </script>
